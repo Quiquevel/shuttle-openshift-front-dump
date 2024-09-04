a23826c6f02660aad8372485c383670e3536104a
Quiquevel <98911999+Quiquevel@users.noreply.github.com>
2024-09-04 11:47:27 +0200

    Add files via upload

--- app/routes/openshift.py
+++ app/routes/openshift.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter, HTTPException, Header
 from pydantic import BaseModel, validator
 from middleware.authorization import is_devops
+from functions.dyna import getEnvironmentsClustersList, dynatraceTreatment
 
 pod_delete = APIRouter(tags=["v1"])
+alerting_status = APIRouter(tags=["v1"])
+
+environmentList = getEnvironmentsClustersList("spain")
+
 class pod_2_delete(BaseModel):
     functionalEnvironment: str
     cluster: str
@@ -19,7 +24,7 @@ class pod_2_delete(BaseModel):
     #To validate the cluster parameter.
     @validator("cluster")
     def validate_cluster(cls, v):
-        if not any(x in v for x in ["ohe","bks","probks","dmzbbks","ohe","probks","dmzbbks","azure","prodarwin","dmzbdarwin","proohe","dmzbohe","confluent"]):
+        if not any(x in v for x in ["ohe","bks","probks","dmzbbks","dmzbazure","ocp05azure","ohe","probks","dmzbbks","azure","prodarwin","dmzbdarwin","proohe","dmzbohe","confluent"]):
             raise HTTPException(status_code=400, detail=f"{v} value is not valid for cluster")
         return v
     
@@ -36,3 +41,17 @@ class pod_2_delete(BaseModel):
 
 class check_token(BaseModel):
     ldap: str
+
+class DynaModel(BaseModel):
+    functionalEnvironment: str
+    timedyna: str = None
+ 
+    @validator("functionalEnvironment")
+    def validate_environment(cls, v):        
+        if not any(x == v for x in environmentList):
+            raise HTTPException(status_code = 400, detail=f"{v} value is not valid for functionalEnvironment")
+        return v
+    
+@alerting_status.post("/dynatrace", tags = ["v1"], description = "GET DYNATRACE ALERT", response_description = "JSON", status_code = 200)
+async def get_dynatrace_alert(target: DynaModel):
+    return await dynatraceTreatment(functionalEnvironment = target.functionalEnvironment, timedyna = target.timedyna)
